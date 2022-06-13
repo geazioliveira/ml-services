bc128542d0df59eb4f98414201db9979f9c57aaf
Geazi Oliveira <geazioliveira@gmail.com>
2022-06-13 09:49:27 -0300

    Add create many method into service

--- ml-category-service/src/app/categories/category.service.ts
+++ ml-category-service/src/app/categories/category.service.ts
@@ -3,6 +3,7 @@ import { InjectRepository } from '@nestjs/typeorm'
 import { Category } from './category.entity'
 import { Repository } from 'typeorm'
 import { BaseService } from '../../core/services/base.service'
+import { CategoryDto } from './category.dto'
 
 @Injectable()
 export class CategoryService extends BaseService<Repository<Category>, Category> {
@@ -11,5 +12,19 @@ export class CategoryService extends BaseService<Repository<Category>, Category>
     private categoryRepository: Repository<Category>
   ) {
     super(categoryRepository)
+
+    this.relations = ['categories']
+  }
+
+  async createMany(categoryId: number, categories: CategoryDto[]): Promise<Category> {
+    const category = await this.findOne(categoryId)
+    const categoriesEntities: Category[] = []
+
+    for (const entity of categories) {
+      categoriesEntities.push(await this.create(entity))
+    }
+
+    category.categories = categoriesEntities
+    return this.categoryRepository.manager.save(category)
   }
 }
